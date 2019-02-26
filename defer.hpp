542e86225dcb4ababe987cdbff5847f44504b951
dongshimou <dongshimou@gmail.com>
2019-02-26 11:23:35 +0800

    Update defer.hpp

--- defer.hpp
+++ defer.hpp
@@ -9,8 +9,11 @@ struct defer{
     template <typename F,typename... Args>
     defer(F&&f,Args&&...args)noexcept{
         auto tmp=std::make_tuple(std::forward<Args>(args)...);
-        task=[&,tt=std::move(tmp)](){
-            std::apply(std::forward<F>(f),tt);
+//         task=[&,tt=std::move(tmp)](){
+//             std::apply(std::forward<F>(f),tt);
+//         };
+        task=[&](){
+            std::apply(std::forward<F>(f),std::move(tmp));
         };
     }
     std::function<void(void)>task;
